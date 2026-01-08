a58e1cee05d76abcca1362f0c8346c8bb61fc9af
rgandolfi <riccardogandi95@gmail.com>
2026-01-08 16:29:16 +0100

    Add opentitan-cluster target

---
+++ configs/opentitan-cluster.sh
@@ -0,0 +1,12 @@
+export PULPRT_TARGET=opentitan-cluster
+export PULPRUN_TARGET=opentitan-cluster
+
+if [  -n "${ZSH_VERSION:-}" ]; then
+        DIR="$(readlink -f -- "${(%):-%x}")"
+        scriptDir="$(dirname $DIR)"
+else
+
+    scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+fi
+
